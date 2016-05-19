3e8bb1ed1b66d1974ffc1481cf46f894174c427c
Santiago Suárez Díaz <exalurt@hotmail.com>
2016-05-19 20:27:23 +0100

    ahora

--- src/reforest.js
+++ src/reforest.js
@@ -1,34 +1,38 @@
 const TelegramBot = require('node-telegram-bot-api');
 const tesseract = require('node-tesseract');
+const PaserLines = require('./PaserLines');
 let _bot;
 module.exports = class reforest{
     constructor(token){
         _bot = new TelegramBot(token, {polling: true});
-        
+
         _bot.on('text', this.messages);
         _bot.on('message', this.recivePhoto);
     }
-    
+
     messages(msg){
         var chatId = msg.chat.id;
         console.log("esto es la hostia: "+msg);
         _bot.sendMessage(chatId,'peroooo');
 
     }
-    
+
     recivePhoto (msg) {
         var chatId = msg.chat.id;
         console.log(msg.document);
         _bot.downloadFile(msg.document.file_id, __dirname).then(function (resp) {
             console.log(resp);
             tesseract.process(resp,function(err,text){
-                if(err){
-                    _bot.sendMessage(chatId,"esta foto no valia lo siento");
-                    }else{
-                        console.log(text);
-                        _bot.sendMessage(chatId,text);
-                    }
-                });
+              console.log(text);
+              if(err){
+                _bot.sendMessage(chatId,"esta foto no valia lo siento");
+              }else{
+                const paserLines = new PaserLines("pueba");
+                const profile = paserLines.scannerLine(text.split("\n"));
+                console.log(profile);
+                _bot.sendMessage(chatId,text);
+              }
+            });
             _bot.sendMessage(chatId,"pero queeeeee");
         });
     }
--- test/PaserLinesTest.js
+++ test/PaserLinesTest.js
@@ -1,77 +1,78 @@
 const expect    = require("chai").expect;
 const PaserLines = require("../build/PaserLines");
 function datos(){
-  var result =[];
-  result.push("-’ JRE AGENT INTEL MISSIL");
-  result.push("");
-  result.push("KeIIyKap0wski <");
-  result.push("LVL 15");
-  result.push("36,301,849 AP 140,000,000 AP");
-  result.push("");
-  result.push("EEK” g 4/4 02/2");
-  result.push("");
-  result.push("/ \\");
-  result.push("«W» .9 \\");
-  result.push("\m/ ,\\");
-  result.push("xn");
-  result.push(",\ r,\ V");
-  result.push("");
-  result.push(" ");
-  result.push("");
-  result.push(" ");
-  result.push("");
-  result.push("A");
-  result.push("V");
-  result.push("");
-  result.push("@QQQQQ");
-  result.push("");
-  result.push("ALLTIME 1 MONTH 1 WEEK 1 NOW");
-  result.push("");
-  result.push("i");
-  result.push("");
-  result.push("Unique Portals Visited 5,019");
-  result.push("Portals Discovered 1");
-  result.push("XM Collected 144,700,906 XM");
-  result.push("1:");
-  result.push("");
-  result.push("Distance Walked 2,184 km");
-  result.push("1:");
-  result.push("");
-  result.push("Resonators Deployed 54,719");
-  result.push("Links Created 8,896");
-  result.push("Control Fields Created 4,747");
-  result.push("Mind Units Captured 10,642,121 MUs");
-  result.push("Longest Link Ever Created 292 km");
-  result.push("Largest Control Field 3,082,190 MUs");
-  result.push("XM Recharged 79,158,869 XM");
-  result.push("Portals Captured 7,314");
-  result.push("Unique Portals Captured 2,955");
-  result.push("Mods Deployed 8,844");
-  result.push("I");
-  result.push("");
-  result.push("Resonators Destroyed 34,120");
-  result.push("Portals Neutralized 5,316");
-  result.push("Enemy Links Destroyed 6,005");
-  result.push("Enemy Control Fields Destroyed 3,014");
-  result.push("i");
-  result.push("");
-  result.push("Max Time Portal Held 326 days");
-  result.push("Max Time Link Maintained 113 days");
-  result.push("Max Link Length x Days 727 km-days");
-  result.push("Max Time Field Held 113 days");
-  result.push("Largest Field MUs x Days 318,818 MU-days");
-  result.push("I");
-  result.push("");
-  result.push("Unique Missions Completed 42");
-  result.push("I");
-  result.push("");
-  result.push("Hacks 27,202");
-  result.push("Glyph Hack Points 98,183");
-  result.push("Longest Hacking Streak 426 days");
-  result.push("i");
-  result.push("");
-  result.push("Agents Successfully Recruited 2");
+  var str = "-’ JRE AGENT INTEL MISSIL\n\
+\n\
+KellyKap0wski <\n\
+LVL 15\n\
+36,301,849 AP 140,000,000 AP\n\
+\n\
+EEK” g 4/4 02/2\n\
+\n\
+/ \\\n\
+«W» .9 \\\n\
+\m/ ,\\\n\
+xn\n\
+\\ r,\\ V\n\
+\n\
+ \n\
+\n\
+ \n\
+\n\
+A\n\
+V\n\
+\n\
+@QQQQQ\n\
+º\n\
+ALLTIME 1 MONTH 1 WEEK 1 NOW\n\
+\n\
+i\n\
+\n\
+Unique Portals Visited 5,019\n\
+Portals Discovered 1\n\
+XM Collected 144,700,906 XM\n\
+1:\n\
+\n\
+Distance Walked 2,184 km\n\
+1:\n\
+\n\
+Resonators Deployed 54,719\n\
+Links Created 8,896\n\
+Control Fields Created 4,747\n\
+Mind Units Captured 10,642,121 MUs\n\
+Longest Link Ever Created 292 km\n\
+Largest Control Field 3,082,190 MUs\n\
+XM Recharged 79,158,869 XM\n\
+Portals Captured 7,314\n\
+Unique Portals Captured 2,955\n\
+Mods Deployed 8,844\n\
+I\n\
+\n\
+Resonators Destroyed 34,120\n\
+Portals Neutralized 5,316\n\
+Enemy Links Destroyed 6,005\n\
+Enemy Control Fields Destroyed 3,014\n\
+i\n\
+\n\
+Max Time Portal Held 326 days\n\
+Max Time Link Maintained 113 days\n\
+Max Link Length x Days 727 km-days\n\
+Max Time Field Held 113 days\n\
+Largest Field MUs x Days 318,818 MU-days\n\
+I\n\
+\n\
+Unique Missions Completed 42\n\
+I\n\
+\n\
+Hacks 27,202\n\
+Glyph Hack Points 98,183\n\
+Longest Hacking Streak 426 days\n\
+i\n\
+\n\
+Agents Successfully Recruited 2";
 
+
+  var result = str.split("\n");
   return result;
 }
 describe("PaserLines", function() {
@@ -79,8 +80,7 @@ describe("PaserLines", function() {
         it("not null", function() {
             const parserLine = new PaserLines("el reto");
             const as = parserLine.scannerLine(datos());
-            console.log(as);
-            expect(as).to.equal(null);
+            expect(as).to.be.not.null;
         });
     });
 });
