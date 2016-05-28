79fc5c03d052c9d6e490acf7dc400a2595a04c92
Santiago Suárez Díaz <exalurt@hotmail.com>
2016-05-28 20:39:40 +0100

    user event relation

--- models/event.js
+++ models/event.js
@@ -9,6 +9,11 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        Event.belongsToMany(models.User,{
+          foreignKey:'id_event',
+          as:'users',
+          through:'events_users'
+        });
       }
     }
   });
--- models/user.js
+++ models/user.js
@@ -14,6 +14,11 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        User.belongsToMany(models.Event,{
+          foreignKey:'id_user',
+          as:'events',
+          through:'events_users'
+        });
       }
     }
   });
